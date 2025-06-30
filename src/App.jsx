7454e1a916738b93b316d52bdb4690ce5cd47d7d
YashKadam05 <kadam.a.yash.20@gmail.com>
2025-06-30 14:34:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,9 @@ function App() {
     <>
       {(!first && <Loader />)}
       {first && (<Routes>
-        <Route path="/mystudio" element={<><Navbar /><Hero /><Service /><Investment /><Booking /></>} />
-        <Route path="/mystudio/gallery" element={<Gallery />} />
-        <Route path="/mystudio/about" element={<><FormBooking /><Booking /></>} />
+        <Route path="/mystudio/" element={<><Navbar /><Hero /><Service /><Investment /><Booking /></>} />
+        <Route path="/mystudio/gallery/" element={<Gallery />} />
+        <Route path="/mystudio/about/" element={<><FormBooking /><Booking /></>} />
       </Routes>)}
       {(first && <Footer />)}
     </>
